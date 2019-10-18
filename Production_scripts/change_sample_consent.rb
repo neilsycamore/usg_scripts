4e50efeabf4bd85b2badd64da549878818ed5b39
Neil <nts@sanger.ac.uk>
2019-10-18 12:07:24 +0100

    withdraw consent for named samples

--- Production_scripts/change_sample_consent.rb
+++ Production_scripts/change_sample_consent.rb
@@ -2,9 +2,12 @@ def change_sample_consent(sample_names)
   ActiveRecord::Base.transaction do
     sample_names.each do |sample_name|
       sample = Sample.find_by(name: sample_name)
-      puts "Unable to find sample with name #{sample_name}" if sample.nil?
-      sample.update_attributes!(:consent_withdrawn => true)
-      puts "#{sample_name} => #{sample.consent_withdrawn}"
+  	  if sample.nil?
+        puts "Unable to find sample with name #{sample_name}"
+  	  else
+        sample.update_attributes!(:consent_withdrawn => true)
+        puts "#{sample_name} => #{sample.consent_withdrawn}"
+  	  end
     end
   end
 end
