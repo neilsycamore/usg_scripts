8478bab8b7aa754d6fb62f26ceab1c529941bc4c
Neil <nts@sanger.ac.uk>
2020-12-02 09:02:29 +0000

    updates..

--- Production_scripts/change_sample_consent.rb
+++ Production_scripts/change_sample_consent.rb
@@ -1,13 +1,15 @@
-def change_sample_consent(sample_names)
+class Sample
+  def add_comment(rt_number, user)
+    Comment.create!(title: "Consent withdrawn",commentable_id: self.id, commentable_type: self.class.name, user_id: user.id, description: "Request via RT#{rt_number}")
+  end
+end
+
+def withdraw_sample_consent_and_add_comment(sample_names,user_login,rt_number)
   ActiveRecord::Base.transaction do
-    sample_names.each do |sample_name|
-      sample = Sample.find_by(name: sample_name)
-  	  if sample.nil?
-        puts "Unable to find sample with name #{sample_name}"
-  	  else
-        sample.update_attributes!(:consent_withdrawn => true)
-        puts "#{sample_name} => #{sample.consent_withdrawn}"
-  	  end
-    end
+    user = User.find_by(login: user_login)
+    Sample.where(name: sample_names).each do |sample|
+      sample.update_attributes!(consent_withdrawn: true, date_of_consent_withdrawn: Date.today, user_id_of_consent_withdrawn: user.id)
+      sample.add_comment(rt_number, user)
+    end; nil
   end
 end
